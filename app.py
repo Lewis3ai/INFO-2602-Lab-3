6d14e8b6f47e18e0c4b0fc41c0bb94ae2541022f
Lewis3ai <158581435+Lewis3ai@users.noreply.github.com>
2025-02-28 18:58:30 +0000

    task 3.1

--- app.py
+++ app.py
@@ -49,6 +49,14 @@ def index():
   return '<h1>mY Todo API</h1>'
 
 # Task 3.1 Here
+def login_user(username, password):
+  user = User.query.filter_by(username=username).first()
+  if user and user.check_password(password):
+    token = create_access_token(identity=username)
+    response = jsonify(access_token=token)
+    set_access_cookies(response, token)
+    return response
+  return jsonify(message="Invalid username or password"), 401
 
 # Task 3.2 Here
 
